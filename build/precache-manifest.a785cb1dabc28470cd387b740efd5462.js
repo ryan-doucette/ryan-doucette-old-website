c014aca70fc99c3af01a3ac1d2b2f9b47269ee8e
ryandoucette <doucette.ry@husky.neu.edu>
2020-12-07 22:45:50 -0500

    Prep for deployment

---
+++ build/precache-manifest.a785cb1dabc28470cd387b740efd5462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d26ec8a892f644756ef43b99b5d4ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a22395ba3d76ba40767",
+    "url": "/static/css/main.e122f822.chunk.css"
+  },
+  {
+    "revision": "b17012630deca1ba4b02",
+    "url": "/static/js/2.5f4e4399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f4e4399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a22395ba3d76ba40767",
+    "url": "/static/js/main.416081f9.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  },
+  {
+    "revision": "09440d3fac918e459be6a0e95ca0ad54",
+    "url": "/static/media/outer_circle2.09440d3f.png"
+  }
+]);
